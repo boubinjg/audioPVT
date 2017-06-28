1df763a5adfa148463609874a2bacfcd40e7d2b3
boubinjg <boubinjg@gmail.com>
2017-06-28 15:08:45 -0400

    breaks thread after 30 seconds

--- audioPVT.py
+++ audioPVT.py
@@ -7,7 +7,7 @@ import thread
 #This is a list of the reaction times (ms) for each key press
 reactionTimes = [];
 #duration of the entire test in seconds
-testDuration = 3*60;
+testDuration = 45;
 timeout = time.time() + testDuration;
 #maximum length the beep (high or low pitch) will sound before timing out
 soundMaxLength = 30;
@@ -25,6 +25,8 @@ def input_thread(list):
     #flush buffer incase of leftover keypresses
     while msvcrt.kbhit():
         msvcrt.getch()
+
+    threadBegin = time.time()
     #wait for key press
     while True:
       if msvcrt.kbhit():
@@ -34,18 +36,21 @@ def input_thread(list):
         reactionTimes.append((time.time() - beginTime)*1000)
         #exit loop
         break;
+      elif time.time() > threadBegin+soundMaxLength:
+        list.append(None)
+        reactionTimes.append(30000)
+        break;
     #flush buffer incase of multiple key presses
     while msvcrt.kbhit():
         msvcrt.getch()
 
 #loop until timeout
 while time.time() < timeout:
-
   #get a random integer between 5 and 10 representing the length of silence (s)
   silence = randint(5,10)
   #sleep for a number of seconds equal to 'silence'
   time.sleep(silence)
-  #store the timeout for the total sound 
+  #store the timeout for the total sound
   maxTime = time.time() + soundMaxLength
   #store the timeout for the low sound
   maxLowSound = time.time() + lowsound;
@@ -80,9 +85,10 @@ while time.time() < timeout:
       time.sleep(waitlength)
 
 #print reaction times
-
 output = open("audioPVToutput.txt","w+")
 for s in reactionTimes:
-  output.write(s)
+  output.write(str(s))
   output.write("\n")
 output.close()
+sleep(5)
+
